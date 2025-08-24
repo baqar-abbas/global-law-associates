06cebd114e64865e05b07a0efbb731339e2686e7
baqar-abbas <baqar.abbas@hotmail.com>
2025-08-25 00:24:21 +0500

    Implement routing using react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/HeaderComponent/Navbar";
+import Layout from "./layouts/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Team from "./pages/Team";
+import Insights from "./pages/Insights";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <main>
-        <section id="home" style={{ height: "100vh", padding: "2rem" }}>
-          <h1>Homepage Hero Coming Next...</h1>
-        </section>
-      </main>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="services" element={<Services />} />
+          <Route path="team" element={<Team />} />
+          <Route path="insights" element={<Insights />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
     </>
   );
 }
