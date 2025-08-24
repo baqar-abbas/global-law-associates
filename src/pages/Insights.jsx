65053763a13fd91b1d158c948a985f7f3911c24f
baqar-abbas <baqar.abbas@hotmail.com>
2025-08-25 00:28:24 +0500

    Create Shared Layout, Stun pages and implement routing using react router and Navlink

---
+++ src/pages/Insights.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Insights = () => {
+  return (
+    <section className="page">
+      <h1>Insights Page</h1>
+    </section>
+  );
+};
+
+export default Insights;
