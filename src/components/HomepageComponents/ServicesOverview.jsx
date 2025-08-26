3b6981894b9017ab5302c242cfdbd5b3f574afb2
baqar-abbas <baqar.abbas@hotmail.com>
2025-08-26 15:27:13 +0500

    Develop stub component for Services overview section

---
+++ src/components/HomepageComponents/ServicesOverview.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  Scale,
+  Briefcase,
+  FileText,
+  Building,
+  Globe,
+  ShieldCheck,
+} from "lucide-react";
+import "./ServicesOverview.css";
+
+const ServicesOverview = () => {
+  return (
+    <div>
+      <h2>Services Overview Section</h2>
+    </div>
+  );
+};
+
+export default ServicesOverview;
