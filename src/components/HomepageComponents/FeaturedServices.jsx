b5b2b43a4409795ddd63434ec55cb244d87abb72
baqar-abbas <baqar.abbas@hotmail.com>
2025-08-28 01:40:44 +0500

    Create stub component for Featured services

---
+++ src/components/HomepageComponents/FeaturedServices.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { CheckCircle } from "lucide-react";
+import { motion } from "framer-motion";
+import "./FeaturedServices.css";
+
+const FeaturedServices = () => {
+  return (
+    <div>
+      <h2>Featured Services Section</h2>
+    </div>
+  );
+};
+
+export default FeaturedServices;
