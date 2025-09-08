a9cf6f3bfdae61a04fd52226ae71f74f06337261
baqar-abbas <baqar.abbas@hotmail.com>
2025-09-08 21:44:13 +0500

    Create stub component and styling css file for the footer

---
+++ src/components/FooterComponent/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Facebook, Linkedin, Twitter, Instagram } from "lucide-react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h2>Footer Component</h2>
+    </footer>
+  );
+};
+
+export default Footer;
