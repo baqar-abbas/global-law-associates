680c520ab8d0d1820965b39bb5e2d5962d6f12e9
baqar-abbas <baqar.abbas@hotmail.com>
2025-09-11 04:30:24 +0500

    Create Stub component for Who We are section

---
+++ src/components/AboutpageComponents/WhoWeAre.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const WhoWeAre = () => {
+  return (
+    <div>
+      <h2>Who We Are</h2>
+      <p>
+        We are a team of dedicated professionals committed to providing
+        top-notch legal and corporate advisory services.
+      </p>
+    </div>
+  );
+};
+
+export default WhoWeAre;
