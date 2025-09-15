8384aa8e113cfa49864a26988c4ce614fdf8d943
baqar-abbas <baqar.abbas@hotmail.com>
2025-09-16 02:40:48 +0500

    Develop How it works section of the services page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ServicesHero from "../components/ServicesPageComponents/ServicesHero";
 import ServicesDetails from "../components/ServicesPageComponents/ServicesDetails";
+import HowItWorks from "../components/ServicesPageComponents/HowItWorks";
+import CallToAction from "../components/HomepageComponents/CallToAction";
 
 const Services = () => {
   return (
     <section>
       <ServicesHero />
       <ServicesDetails />
+      <HowItWorks />
+      <CallToAction />
     </section>
   );
 };
