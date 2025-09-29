c8345aaae232af454cb8d72089eb5d19327fc58a
baqar-abbas <baqar.abbas@hotmail.com>
2025-09-29 20:05:33 +0500

    Develop Blogs/ Article grid section of the Insights page which shows the articles in Popup Modal

--- src/pages/Insights.jsx
+++ src/pages/Insights.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import InsightsHero from "../components/InsightsPageComponents/InsightsHero";
+import InsightsArticles from "../components/InsightsPageComponents/InsightsArticles";
 
 const Insights = () => {
   return (
     <section>
       <InsightsHero />
-      <h1>Insights Page</h1>
+      <InsightsArticles />
     </section>
   );
 };
